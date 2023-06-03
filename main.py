606c951c04ca3b3dba2d9fc029abbadf0080e158
Vladislav Antonov <lolik1806@gmail.com>
2023-06-03 08:19:03 +0300

    train model

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from keras.datasets import mnist
 import numpy as np
+import time
 from neural import Network, load_image_cv
 from tkinter import *
 from tkinter import filedialog
+from tkinter import messagebox
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
                                                NavigationToolbar2Tk)
 
+from multiprocessing import Process, Manager, Barrier, cpu_count, current_process
+
 
 def main():
     gui = GUI()
-    gui.load_mnist_data()
     gui.start()
 
 
@@ -52,17 +55,26 @@ class GUI:
         self.y_train = None
         self.y_test = None
 
+        self.max_train_size = 0
+        self.max_test_size = 0
+
         self.result = StringVar()
         self.threads_count = IntVar()
 
+        self.load_mnist_data()
+
         self.buttons_frame = Frame(self.__root, width=50, pady=10, padx=10)
+        self.result_frame = Frame(self.__root, width=50, padx=10, pady=10)
+        self.max_params_frame = Frame(self.__root, padx=5, pady=5)
+        self.train_frame = Frame(self.__root, padx=5, pady=5)
+        self.threads_counter_frame = Frame(self.__root, padx=5, pady=5)
+
         self.load_img_btn = Button(self.buttons_frame, text="Загрузить изображение", padx=2, pady=2, width=30, height=1,
                                    bg='white', fg='black', command=self.load_image_from_filesystem)
-
         self.create_model_btn = Button(self.buttons_frame, text="Создать модель", padx=2, pady=2,
                                        width=30, height=1, bg='white', fg='black', command=self.create_model)
-        self.train_model_btn = Button(self.buttons_frame, text="Обучить модель", padx=2, pady=2,
-                                      width=30, height=1, bg='white', fg='black')
+        self.train_model_btn = Button(self.threads_counter_frame, text="Начать обучение модель", padx=2, pady=2,
+                                      width=30, height=1, bg='white', fg='black', command=self.train_model)
         self.save_model_btn = Button(self.buttons_frame, text="Сохранить модель", padx=2, pady=2,
                                      width=30, height=1, bg='white', fg='black', command=self.save_model)
         self.show_model_info_btn = Button(self.buttons_frame, text='Показать информацию о модели', padx=2, pady=2,
@@ -76,6 +88,22 @@ class GUI:
         self.show_train_btn = Button(self.buttons_frame, text="Тренировочные данные", padx=2, pady=2,
                                      width=30, height=1, bg='white', fg='black', command=self.show_mnist)
 
+        self.result_label = Label(self.result_frame, text="", font=("Arial", 16))
+
+        self.max_threads_count_label = Label(self.max_params_frame,
+                                             text=f"Доступное количество потоков: {cpu_count()}",
+                                             font=("Arial", 10))
+
+        self.max_train_size_label = Label(self.max_params_frame,
+                                          text=f"Доступный размер обучающих данных: {len(self.x_train)}",
+                                          font=("Arial", 10))
+
+        self.train_size_label = Label(self.train_frame, text="Размер обучающих данных", font=("Arial", 10))
+        self.train_size_entry = Entry(self.train_frame)
+
+        self.threads_count_label = Label(self.threads_counter_frame, text="Количество потоков", font=("Arial", 10))
+        self.threads_count_entry = Entry(self.threads_counter_frame)
+
         self.image_plot = None
         self.canvas = None
 
@@ -84,6 +112,20 @@ class GUI:
         self.__root.geometry("1280x720+50+50")
 
         self.buttons_frame.pack(anchor=NW, side=LEFT)
+
+        self.max_params_frame.pack(anchor=NW, side=TOP, expand=FALSE)
+        self.max_threads_count_label.pack(anchor=NW, side=TOP)
+        self.max_train_size_label.pack(anchor=NW, side=TOP)
+        self.train_model_btn.pack(anchor=NW, side=BOTTOM)
+
+        self.train_frame.pack(anchor=NW, side=TOP, expand=FALSE)
+        self.train_size_label.pack(anchor=NW, side=LEFT)
+        self.train_size_entry.pack(anchor=NE, side=RIGHT)
+
+        self.threads_counter_frame.pack(anchor=NW, side=TOP, expand=FALSE)
+        self.threads_count_label.pack(anchor=NW, side=LEFT)
+        self.threads_count_entry.pack(anchor=NE, side=RIGHT)
+
         self.load_img_btn.pack(anchor=NW)
         self.load_model_btn.pack(anchor=NW)
         self.create_model_btn.pack(anchor=NW)
@@ -97,6 +139,9 @@ class GUI:
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(anchor=NE, side=TOP)
 
+        self.result_frame.pack(anchor=NE)
+        self.result_label.pack(anchor=NE)
+
         self.__root.mainloop()
 
     def load_model(self):
@@ -109,7 +154,7 @@ class GUI:
     def predict(self):
         res = self.model.predict(self.grayscale_image)
 
-        a = 0
+        self.result_label['text'] = f'Результат: {res}'
 
     def load_image_from_filesystem(self):
         filepath = filedialog.askopenfilename()
@@ -134,6 +179,9 @@ class GUI:
         self.y_train = y_train
         self.y_test = y_test
 
+        self.max_train_size = len(self.x_train)
+        self.max_test_size = len(self.x_test)
+
     def show_mnist(self):
         mnist_win = Tk()
         mnist_win.geometry('1280x720+50+50')
@@ -150,7 +198,7 @@ class GUI:
         canvas.draw()
         canvas.get_tk_widget().pack(anchor=CENTER)
 
-    def create_model(self, event):
+    def create_model(self):
         self.model = Network()
         self.model.init_weights()
 
@@ -160,8 +208,104 @@ class GUI:
         if filepath != "":
             self.model.save_model(filepath)
 
+    def train_model(self):
+        if self.model is None:
+            messagebox.showerror("Ошибка", "Загрузите или создайте модель!")
+            return
+
+        threads = int(self.threads_count_entry.get())
+        train_size = int(self.train_size_entry.get())
+        pack_size = train_size // threads
+
+        print(f"Data size: {train_size}, Threads: {threads}")
+
+        start_time = time.monotonic()
+
+        images = self.x_train[:train_size].reshape(train_size, 28 * 28) / 255
+        labels = self.y_train[:train_size]
+
+        one_hot_labels = np.zeros((len(labels), 10))
+
+        for i, j in enumerate(labels):
+            one_hot_labels[i][j] = 1
+
+        labels = one_hot_labels
+
+        image_packs = []
+        labels_packs = []
+
+        for i in range(threads):
+            start, end = ((i * pack_size), ((i + 1) * pack_size))
+            image_packs.append(images[start:end])
+            labels_packs.append(labels[start:end])
+
+        models = []
+        for _ in range(threads):
+            model_copy = self.model.copy_model()
+            models.append(model_copy)
+
+        bar = Barrier(threads)
+        processes = []
+
+        start_pr = time.monotonic()
+
+        with Manager() as m:
+            grads_dict = m.dict()
+            weights_dict = m.dict()
+
+            for i in range(threads):
+                pr = Process(name=f"proc-{i}", target=models[i].fit,
+                             args=(
+                                 image_packs[i], labels_packs[i], 16, 40, bar, grads_dict, weights_dict, 0.1, 0.1,
+                                 print_proc))
+                processes.append(pr)
+
+                pr.start()
+
+            end_pr = time.monotonic() - start_pr
+            print(end_pr)
+
+            for p in processes:
+                p.join()
+
+            for i in range(threads):
+                key = f"proc-{i}"
+
+                weights = weights_dict[key]
+
+                models[i].weights_0_1 = weights[0]
+                models[i].weights_1_2 = weights[1]
+                models[i].weights_2_3 = weights[2]
+
+        end_time = time.monotonic() - start_time
+        print(end_time)
+
+        test_images = self.x_test.reshape(len(self.x_test), 28 * 28) / 255
+        test_labels = np.zeros((len(self.y_test), 10))
+
+        for i, j in enumerate(self.y_test):
+            test_labels[i][j] = 1
+
+        evals = []
+        max_ev = 0
+        ind = -1
+        for i in range(len(models)):
+            v = models[i].evaluate(test_images, test_labels)
+
+            if v[1] > max_ev:
+                max_ev = v[1]
+                ind = i
+
+        self.model = models[ind].copy_model()
+
     def show_model_info(self):
         pass
+
+
+def print_proc(a, b, c, d, i):
+    g = current_process().name
+
+    # print(f"{g} Epoch: {i} Train-Err: {a} Train-Acc: {b} Validation-Err: {c} Validation-Acc: {d}")
 
 
 if __name__ == '__main__':
--- neural/network.py
+++ neural/network.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 import h5py
 from copy import deepcopy
+from multiprocessing import current_process
 
 
 class Network:
@@ -19,11 +20,13 @@ class Network:
         self.weights_1_2 = 0.2 * np.random.random((self.hidden_size, self.hidden_size)) - 0.1
         self.weights_2_3 = 0.2 * np.random.random((self.hidden_size, self.output_size)) - 0.1
 
-    def fit(self, x_train, y_train, batch_size, epochs, validation_split=0.1, alpha=0.01):
+    def fit(self, x_train, y_train, batch_size, epochs, bar, grads_dict, weights_dict, validation_split=0.1, alpha=0.01,
+            output_func=None):
+
         validation_size = int(len(x_train) * validation_split)
         train_size = int(len(x_train) - validation_size)
 
-        total_start = time.monotonic()
+        # total_start = time.monotonic()
 
         for e in range(epochs):
             error, correct_cnt = (0.0, 0)
@@ -37,9 +40,11 @@ class Network:
             validation_images = x_train[train_size:]
             validation_labels = y_train[train_size:]
 
-            start = time.monotonic()
+            # start = time.monotonic()
 
-            for i in range(len(train_images)):
+            batch_count = len(train_images) // batch_size
+
+            for i in range(batch_count):
                 batch_start, batch_end = ((i * batch_size), ((i + 1) * batch_size))
 
                 layer_0 = train_images[batch_start:batch_end]
@@ -49,13 +54,37 @@ class Network:
                 # layer_1 *= dropout_mask * 2
                 layer_3 = softmax(np.dot(layer_2, self.weights_2_3))
 
+                error += np.sum((train_labels[batch_start:batch_end] - layer_3) ** 2)
+
                 for k in range(layer_3.shape[0]):
                     correct_cnt += int(
                         np.argmax(layer_3[k:k + 1]) == np.argmax(train_labels[batch_start + k:batch_start + k + 1]))
 
-                layer_3_delta = (train_labels[batch_start:batch_end] - layer_3) / (batch_size * layer_3.shape[0])
+                layer_3_delta = (train_labels[batch_start:batch_end] - layer_3) / batch_size
                 layer_2_delta = layer_3_delta.dot(self.weights_2_3.T) * tanh2deriv(layer_2)
                 layer_1_delta = layer_2_delta.dot(self.weights_1_2.T) * tanh2deriv(layer_1)
+
+                current_proc_name = current_process().name
+
+                grads_dict[current_proc_name] = (layer_3_delta, layer_2_delta, layer_1_delta)
+
+                bar.wait()
+
+                dict_len = len(grads_dict)
+
+                layer_3_delta = np.zeros(layer_3_delta.shape)
+                layer_2_delta = np.zeros(layer_2_delta.shape)
+                layer_1_delta = np.zeros(layer_1_delta.shape)
+
+                for k, v in grads_dict.items():
+                    layer_3_delta += v[0]
+                    layer_2_delta += v[1]
+                    layer_1_delta += v[2]
+
+                layer_3_delta /= dict_len
+                layer_2_delta /= dict_len
+                layer_1_delta /= dict_len
+
                 # layer_1_delta *= dropout_mask
 
                 self.weights_2_3 += alpha * layer_2.T.dot(layer_3_delta)
@@ -68,17 +97,25 @@ class Network:
                 layer_2 = tanh(np.dot(layer_1, self.weights_1_2))
                 layer_3 = softmax(np.dot(layer_2, self.weights_2_3))
 
+                test_error += np.sum((validation_labels[i:i + 1] - layer_3) ** 2)
+
                 test_correct_cnt += int(np.argmax(layer_3) == np.argmax(validation_labels[i:i + 1]))
 
-            end = time.monotonic() - start
+            # end = time.monotonic() - start
 
-            print(
-                f"I:{e} || Validation-Acc:{test_correct_cnt / float(validation_size)} "
-                f"|| Train-Acc:{correct_cnt / float(train_size)} "
-                f"|| {end}s")
+            output_func(error / float(train_size), correct_cnt / float(train_size), test_error /
+                        float(validation_size), test_correct_cnt / float(validation_size), e)
 
-        total_end = time.monotonic() - total_start
-        print(f"Total learning time: {total_end}s")
+            # print(
+            #     f"I:{e} || Validation-Acc:{test_correct_cnt / float(validation_size)} "
+            #     f"|| Train-Acc:{correct_cnt / float(train_size)} "
+            #     f"|| {end}s")
+
+            # total_end = time.monotonic() - total_start
+            # print(f"Total learning time: {total_end}s")
+
+        nn = current_process().name
+        weights_dict[nn] = (self.weights_0_1, self.weights_1_2, self.weights_2_3)
 
     def predict(self, data):
         data = data.reshape(1, self.input_size)
@@ -103,9 +140,12 @@ class Network:
             layer_2 = tanh(np.dot(layer_1, self.weights_1_2))
             layer_3 = softmax(np.dot(layer_2, self.weights_2_3))
 
+            error += np.sum(np.sum((y_test[i:i + 1] - layer_3) ** 2))
             correct_cnt += int(np.argmax(layer_3) == np.argmax(y_test[i:i + 1]))
 
-        print(f"Test-Acc:{correct_cnt / float(len(x_test))}")
+        # print(f"Test-Err:{error / float(len(x_test))} Test-Acc:{correct_cnt / float(len(x_test))}")
+
+        return error / float(len(x_test)), correct_cnt / float(len(x_test))
 
     def load_model(self, file_path):
         with h5py.File(file_path, 'r') as f:
